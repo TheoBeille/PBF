b4a947e7d600b3740deb816b8c52ece60eb9d598
TheoBeille <theo.beille@ensta.fr>
2025-11-07 12:43:11 +0100

    MAJ gros df

--- ML.py
+++ ML.py
@@ -1,7 +1,13 @@
-import pandas as pd
+
 from sklearn.neighbors import BallTree
 import os 
 import numpy as np
+import json
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point, shape
+from shapely import wkt
+
 
 DATA_DIR = "Données_Nettoyées"
 
@@ -57,6 +63,71 @@ if pairs:
 #prend le chantier au niveau max; si égalité, on garde le premier.
 
 
-print((df_acc["perturbation"] != 0).sum()) 
-print(df_acc[df_acc["perturbation"] != 0])
+#print((df_acc["perturbation"] != 0).sum()) 
+#print(df_acc[df_acc["perturbation"] != 0])
 
+
+
+#les acc dans les ame
+
+df_ame["geometry"] = df_ame["geo_shape"].apply(lambda s: shape(json.loads(s)))
+pistes = gpd.GeoDataFrame(df_ame, geometry="geometry", crs="EPSG:4326")
+
+
+
+lat_col = "lat"
+lon_col = "lon"
+
+acc = gpd.GeoDataFrame(
+    df_acc.copy(),
+    geometry=gpd.points_from_xy(df_acc[lon_col], df_acc[lat_col]), #permet de faire des coor (x,y) avec le bon type
+    crs="EPSG:4326"
+)
+
+# Projection métrique pour des distances en mètres
+acc_m    = acc.to_crs(2154)
+pistes_m = pistes.to_crs(2154)
+
+
+# Assure un index unique côté accidents et pistes
+acc_m = acc_m.reset_index(drop=True)
+pistes_m = pistes_m.reset_index(drop=True)
+
+# Jointure plus proche (peut renvoyer plusieurs lignes par accident en cas d’égalité)
+merged = gpd.sjoin_nearest(
+    acc_m, 
+    pistes_m, 
+    how="left", 
+    distance_col="dist_m"
+)
+
+piste_cols = [col for col in merged.columns if col in df_ame.columns or col.endswith("_right")]
+
+mask_loin = merged["dist_m"] > 2
+merged.loc[mask_loin, piste_cols] = None  # met NaN sur les colonnes de piste si accidents > a 2m
+
+
+merged = merged.to_crs(4326)
+
+
+
+
+merged_no_geo = merged.drop(columns="geometry").copy()
+
+cols_to_keep = [
+    "Date", "lat_left","lon_left","Gravité","perturbation",
+    "dist_m",
+    "Côté aménagement","Sens",
+    "Aménagement",
+    "Côté aménagement",
+    "Gravité",
+    
+]
+
+merged_no_geo = merged_no_geo[cols_to_keep]
+
+merged_no_geo['dist_m'] = np.where(merged_no_geo['dist_m'].notna(), merged_no_geo['dist_m'] <= 2, False)
+merged_no_geo =merged_no_geo.rename(columns={"lat_left": "lat", "lat_right": "lon", "dist_m": "ame"})
+
+output_path = "Données_Nettoyées/accidents_avec_amenagement.csv"
+merged_no_geo.to_csv(output_path, index=False, sep=';')
